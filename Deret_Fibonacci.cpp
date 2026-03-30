48fc202ef83df9d156edfe90ab97cf4c0d3b0df8
asokarakha55-cloud <asokarakha55@gmail.com>
2026-03-30 17:46:47 +0700

    Menambahkan fungsi

--- Deret_Fibonacci.cpp
+++ Deret_Fibonacci.cpp
@@ -5,3 +5,11 @@ using namespace std;
 // menambahkan variabel global
 int n;
 int pilihan;
+
+// menambahkan fungsi
+
+bool isprime(int num);
+bool isFibonacci(int num);
+void inputNumber();
+void displayresult(int type);
+void showmenu();
